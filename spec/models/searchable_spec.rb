e3fdad598cfdd3a24dede58ebb1e8a15263bb820
David Asabina <david@supr.nu>
2015-05-07 20:47:06 +0200

    Testing dynamic module class definition

--- spec/models/searchable_spec.rb
+++ spec/models/searchable_spec.rb
@@ -9,6 +9,28 @@ class SearchableDummyModel < ActiveRecord::Base
   column :name, :string
 end
 
+module SearchableTestModels
+  module Nothing
+  end
+end
+
+class Strasse
+  def district=(val); @district=val end
+  def district; @district end
+  def name=(val); @name=val end
+  def name; @name end
+  def self.type; 'street' end
+end
+class Address < Strasse
+  def initialize(a,b)
+    @name=a
+    @number=b
+  end
+  def number=(val); @number=val end
+  def number; @number end
+  def to_s; "Address is #{name} #{number}, #{district}" end
+end
+
 describe 'Searchability' do
   context 'on a dummy model' do
     before do
@@ -24,6 +46,36 @@ describe 'Searchability' do
 
     it 'responds to #email' do
       expect(SearchableDummyModel.new).to respond_to(:email)
+    end
+
+    it 'interrogates objects' do
+      old_address = 'Eichendorffstraße 18'
+      new_address = 'Novalisstraße 12'
+
+      klass = Class.new(Strasse)
+      klass.class_eval do
+        def initialize(a,b)
+          @name=a
+          @number=b
+        end
+        def to_s; "Adresse est #{name} #{@number}, #{district}" end
+      end
+
+      first = Address.new(*old_address.split)
+      expect(first.class.superclass).to equal(Strasse)
+      expect(first.number).to eq(old_address.split.last)
+      expect(first.name).to eq(old_address.split.first)
+      expect(first.class.type).to eq(Strasse.type)
+
+      second = klass.new(*new_address.split)
+      expect(second.class.superclass).to equal(Strasse)
+      expect(second.name).to eq(new_address.split.first)
+      expect(second.class.type).to eq(Strasse.type)
+    end
+
+    it 'is named' do
+      SearchableTestModels.const_set("Dummy", Class.new(Strasse))
+      #p SearchableTestModels::Dummy
     end
   end
 end
