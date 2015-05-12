7152e64e932f96a5b255be35685e784fbafb7175
David Asabina <david@supr.nu>
2015-05-12 12:28:32 +0200

    Make routing so much simpler :)

--- lib/searchengine/rails/routes.rb
+++ lib/searchengine/rails/routes.rb
@@ -1,8 +1,22 @@
 module ActionDispatch::Routing
   class Mapper
     def searchability_for(resource, options={})
-      #get "#{route_format resource}", to: "#{options[:controller]}#query"
-      get "query", to: "#{options[:controller]}#query"
+      get "#{resource}/query", to: "#{options[:controller] || resource}#query" #, as: "#{resource}_query"
+    end
+
+    ##
+    # searchability_for :users # simple way
+    # resources :users do # inside a resources route
+    #   collection do
+    #     searchable
+    #   end
+    # end
+    def searchable
+      if resource_scope?
+        # TODO: raise not yet implemented
+      else
+        get("query", to: "#{parent_resource.controller}#query") if parent_resource # TODO: check for resource_scope? and throw error on failure
+      end
     end
 
     private
--- spec/routes_spec.rb
+++ spec/routes_spec.rb
@@ -6,11 +6,35 @@ end
 describe 'Routes', type: :routing do
   routes { Searchengine::Engine.routes }
 
-  it 'directs a searchability route to the search controller' do
+  it 'directs a searchability route to the specified controller' do
     Searchengine::Engine.routes.draw do
       searchability_for :butterflies, controller: 'butterflies'
     end
-    expect(:get => '/search/butterflies').to route_to(
+    expect(get: '/butterflies/query').to route_to(
+      controller: 'searchengine/butterflies',
+      action: 'query'
+    )
+  end
+
+  it 'directs a searchability route to an implied controller' do
+    Searchengine::Engine.routes.draw do
+      searchability_for :butterflies
+    end
+    expect(get: '/butterflies/query').to route_to(
+      controller: 'searchengine/butterflies',
+      action: 'query'
+    )
+  end
+
+  it 'infers the controller from the resource' do
+    Searchengine::Engine.routes.draw do
+      resources :butterflies do
+        collection do
+          searchable
+        end
+      end
+    end
+    expect(get: '/butterflies/query').to route_to(
       controller: 'searchengine/butterflies',
       action: 'query'
     )
