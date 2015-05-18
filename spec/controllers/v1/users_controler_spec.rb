495f45305e8467da3a3f4295cdf59e7facd1e162
David Asabina <david@supr.nu>
2015-05-18 11:07:08 +0200

    Cleaned up the controller specs

--- spec/controllers/v1/users_controler_spec.rb
+++
@@ -1,6 +0,0 @@
-describe Searchengine::V1::UsersController, type: :controller do
-#describe Searchengine::UsersController, type: :controller do
-  it 'does nothing' do
-    expect(1).to be_truthy
-  end
-end
