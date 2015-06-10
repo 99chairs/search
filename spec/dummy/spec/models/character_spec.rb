77ec34e21689f3ed45c17d91e2c732e5c35d21ab
David Asabina <david@supr.nu>
2015-06-10 10:55:30 +0200

    Sped up characters spec + added fixtures for aggs

--- spec/dummy/spec/models/character_spec.rb
+++ spec/dummy/spec/models/character_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Character, type: :model do
   before(:all) do
-    Chewy.root_strategy = :urgent
+    Character.destroy_all
     populate_dummy_dataset
     wait_until_index_is_populated
   end
@@ -41,6 +41,23 @@ RSpec.describe Character, type: :model do
       { name: 'Julius Caesar', description: 'statesman, general', email: 'julius@caesar.it' },
       { name: 'Nero', description: 'brute', email: 'nero@rome.it' },
       { name: 'Caligula', description: 'promiscuous prick', email: 'cali@la.it' },
+      { name: 'Peter Griffin', description: 'the fat guy', category: 'Family Guy' },
+      { name: 'Stewie Griffin', description: 'evil baby', category: 'Family Guy' },
+      { name: 'Brian Griffin', description: 'talking dog', category: 'Family Guy' },
+      { name: 'Adam West', category: 'Family Guy' },
+      { name: 'Megatron Griffin', category: 'Family Guy' },
+      { name: 'Montogomery Burns', description: 'evil old fart', category: 'The Simpsons' },
+      { name: 'Homer Simpson', description: 'the fat guy', category: 'The Simpsons' },
+      { name: 'Bart Simpson', description: 'eat my shorts', category: 'The Simpsons' },
+      { name: 'Krusty', description: 'class clown', category: 'The Simpsons' },
+      { name: 'Eric Cartman', description: 'the fat guy', category: 'South Park' },
+      { name: 'Chef', description: 'the black guy', category: 'South Park' },
+      { name: 'Token', description: 'the young black guy', category: 'South Park' },
+      { name: 'Kenny', description: 'the jew', category: 'South Park' },
+      { name: 'Kyle', description: 'the jew', category: 'South Park' },
+      { name: 'Kenny', description: 'the poor kid', category: 'South Park' },
+      { name: 'Timmy', description: 'the disabled kid', category: 'South Park' },
+      { name: 'Jimmy', description: 'class clown', category: 'South Park' },
     ]
     characters.each { |attributes| Character.create attributes }
   end
