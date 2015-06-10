fb70caaf5d4b71857b3e968b13f39525601cbd0d
David Asabina <david@supr.nu>
2015-06-10 10:24:43 +0200

    Disabled focus setting + fixed Character specs

--- spec/dummy/spec/models/character_spec.rb
+++ spec/dummy/spec/models/character_spec.rb
@@ -11,18 +11,18 @@ RSpec.describe Character, type: :model do
     Character.destroy_all
   end
 
-  it 'finds Caligula by his full e-mail address' do
+  it 'finds Caligula by his full e-mail address', type: 'search' do
     expect(search_results_for('cali@la.it')).to include(a_hash_including('name' => 'Caligula'))
   end
 
-  ['cali@la', 'cali', 'cali@', 'ali', '.it'].each do |phrase|
-    it "finds Caligula by his partial e-mail address #{phrase}" do
+  ['cali@la', 'cali', 'cali@'].each do |phrase|
+    it "finds Caligula by his e-mail address token #{phrase}", type: 'search' do
       expect(search_results_for(phrase)).to include(a_hash_including('name' => 'Caligula'))
     end
   end
 
-  ['*cali@la.it*', '*cali@la.it', 'cali@la.it*', 'cali*', '*i@la*', '*ali', '*@la', '*@la.it', '*.it', '*it'].each do |phrase|
-    it "finds Caligula by his wildcarded e-mail address #{phrase}" do
+  ['*cali@la.it*', '*cali@la.it', 'cali@la.it*', 'cali*', '*cali*', '*i@la*', '*ali', '*ali*', '*@la', '*@la.it', '*.it', '*.it*', '*it'].each do |phrase|
+    it "finds Caligula by his wildcarded e-mail address #{phrase}", type: 'search' do
       expect(search_results_for(phrase)).to include(a_hash_including('name' => 'Caligula'))
     end
   end
@@ -32,7 +32,7 @@ RSpec.describe Character, type: :model do
   end
 
   def find_by_searchphrase(phrase)
-    q = { query_string: { query: phrase, analyze_wildcard: true } }
+    q = { query_string: { query: "#{phrase}", analyze_wildcard: true } }
     Character.search_type.query(q)
   end
 
@@ -40,20 +40,17 @@ RSpec.describe Character, type: :model do
     characters = [
       { name: 'Julius Caesar', description: 'statesman, general', email: 'julius@caesar.it' },
       { name: 'Nero', description: 'brute', email: 'nero@rome.it' },
-      { name: 'Caligula', description: 'promiscuous killer', email: 'cali@la.it' },
+      { name: 'Caligula', description: 'promiscuous prick', email: 'cali@la.it' },
     ]
     characters.each { |attributes| Character.create attributes }
   end
 
   def wait_until_index_is_populated
-    count = 0
     Character.search_type.import!
-    while Character.search_type.total_count < 3
-      count += 1
-      if count > 10
-        puts "unable to resolve" 
-        exit
-      end
+    count = 10
+    while Character.search_type.total_count != Character.count
+      fail "unable to resolve" if count <= 0
+      count -= 1
     end
   end
 end
